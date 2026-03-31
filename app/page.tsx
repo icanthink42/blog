ff95ef584ee5c4107f89ba97964b96086d2e7679
icanthink42 <icanthink42@gmail.com>
2026-03-31 10:45:37 -0400

    proper blog sorting

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,13 @@ import { allPosts } from "@/.contentlayer/generated"
 import Link from "next/link"
 
 export default function Home() {
+  const sortedPosts = allPosts.sort((a, b) =>
+    new Date(b.date).getTime() - new Date(a.date).getTime()
+  )
+
   return (
     <div className="prose dark:prose-invert">
-      {allPosts.map((post) => (
+      {sortedPosts.map((post) => (
         <article key={post._id}>
           <Link href={post.slug}>
             <h2>{post.title}</h2>
